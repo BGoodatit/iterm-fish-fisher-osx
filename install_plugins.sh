7a14af67a416a7608c6f093bdffcd8d3e60772d3
BGoodatit <81009488+BGoodatit@users.noreply.github.com>
2024-04-03 11:48:27 -0400

    Change theme to oh-my-fish/theme-chain in install_plugins.sh

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 #title         :install_plugins.sh
 #description   :This script will install and configure themes and plugins for Fish Shell
-#author        :ghaiklor
-#date          :2021-06-20
-#version       :1.0
-#usage         :curl --silent --location "https://github.com/ghaiklor/iterm-fish-fisher-osx/blob/master/install_plugins.sh?raw=true" | fish
+#author        :BGoodatit
+#date          :2024-04-03
+#version       :1.1
+#usage         :curl --silent --location "https://github.com/BGoodatit/iterm-fish-fisher-osx/blob/askthecode/install_plugins.sh?raw=true" | fish
 #fish_version  :3.2.2
 #===================================================================================
 
@@ -13,13 +13,20 @@ brew install terminal-notifier grc
 
 fisher install jorgebucaran/fisher
 fisher install edc/bass
-fisher install oh-my-fish/theme-bobthefish
+fisher install oh-my-fish/theme-chain
 fisher install patrickf1/colored_man_pages.fish
 fisher install franciscolourenco/done
 fisher install oh-my-fish/plugin-grc
 fisher install jorgebucaran/nvm.fish
 fisher install oh-my-fish/plugin-pj
 fisher install markcial/upto
+
+set --universal --export theme_nerd_fonts yes
+set --universal --export theme_color_scheme zenburn
+set --universal --export PROJECT_PATHS ~/Library/Projects
+
+fish_update_completions
+
 
 set --universal --export theme_nerd_fonts yes
 set --universal --export theme_color_scheme zenburn
